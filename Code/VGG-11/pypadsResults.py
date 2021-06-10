06298886ce0305c5054b42ca9cabd8301692ddc9
pandeysh <pandey02@ads.uni-passau.de>
2021-06-10 12:40:01 +0200

    get pypads dataframe

--- Code/VGG-11/pypadsResults.py
+++ Code/VGG-11/pypadsResults.py
@@ -4,11 +4,6 @@ import numpy as np
 import os
 import json
 
-#os.system("export MONGO_DB = pypads")
-#os.system("export MONGO_USER = pypads")
-#os.system("export MONGO_URL = 'mongodb://www.padre-lab.eu:2222'")
-#os.system("export MONGO_PW = 8CN7OqknwhYr3RO")
-
 os.environ['MONGO_DB'] = "pypads"
 
 os.environ['MONGO_USER'] = "pypads"
@@ -21,25 +16,13 @@ os.environ['MONGO_PW'] = "8CN7OqknwhYr3RO"
 tracker = base.PyPads(autostart=True)
 obj1 = results.PyPadsResults()
 
-#obj1.get_tracked_objects()
 res = obj1.get_experiment(experiment_name="Effect of GPUs - Logistic map")
 print('Exp details', res)
+#experiment_ids='7'
 
-res2 = obj1.get_experiments_data_frame(experiment_names="Effect of GPUs - Logistic map", experiment_ids='8')
+res2 = obj1.get_experiments_data_frame(experiment_names="Effect of GPUs - Logistic map")
 print('Output', res2)
-print('type of ', type(res2))
-#with open('pypads.json', 'w') as outfile:
-#    json.dump(res2, outfile)
+df = pd.DataFrame(res2)
+print('DF ', df)
 
-res2 = res2.values[0]
-
-np.savetxt("foo.csv", res2, delimiter=",")
-#res2.to_csv('OutputToCSV.csv')
-#data1 = pd.DataFrame(res) 
-#print('dataFrame', data1)
-
-
-#mask = np.column_stack([res2[col].str.contains(r".rgb", na=False) for col in res2])
-#print('mask ', res2.loc[mask.any(axis=1)])
-
-
+np.savetxt(r'np2.txt', df.values, fmt='%s')
