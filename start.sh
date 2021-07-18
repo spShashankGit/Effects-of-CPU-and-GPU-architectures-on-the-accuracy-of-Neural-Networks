0f834e6aa3ac9755f49b5936d2ff4ff1c1a855db
pandeysh <pandey02@ads.uni-passau.de>
2021-07-18 14:49:13 +0000

    python version;
    compare.py added;
    
    instuction to install correct python version added;
    
    instruction to make 3.8 the default python version added

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directory name
-APP_NAME="thesis_experiment"
+APP_NAME="thesis_experiment_shashank"
 
 cd
 mkdir $APP_NAME
@@ -11,17 +11,19 @@ cd $APP_NAME
 #conda create -n thesisExpCondaEnv python=3.6
 #conda activate thesisExpCondaEnv 
 
-conda create -n thesisExpCondaEnv_P38 python=3.8
-conda activate thesisExpCondaEnv_P38
+#conda create -n thesisExpCondaEnv_P38 python=3.8
+#conda activate thesisExpCondaEnv_P38
 
 # Install conda packages from the req file
-conda install --file requirements_gpuInfoLogger1.txt -y
+pip install --file requirements_gpuInfoLogger1.txt -y
 
 # Install pycuda via pip
 python3 -m pip install pycuda==2020.1
 
+
 # Install pytorch 
 conda install -c pytorch pytorch 
+
 
 # Set environment variables for pypads logging
 export MONGO_DB=pypads;
@@ -29,10 +31,30 @@ export MONGO_USER=pypads;
 export MONGO_URL=mongodb://ilz.dimis.fim.uni-passau.de:29642;
 export MONGO_PW=8CN7OqknwhYr3RO;
 
+
+# Update to Python 3.8.0
+apt update
+sudo apt install software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
+apt update
+apt install python3.8
+python3 --version
+
+#.bashrc
+#alias="/usr/bin/python3.8"
+#https://medium.com/analytics-vidhya/installing-python-3-8-3-66701d3db134
+
+
+# Check if all packages are installed correctly
+pip freeze >> pipFreeze.txt
+python3 compare.py
+
+
 # Steps to run the experiment
-#python3 -m pip install Code/PyPads_GPU_Details_Logger/dist/pypads-0.5.7_with_env_variable.tar.gz
-#python3 Code/Logistic_Map/Logistic_Map_Script.py
-#python3 Code/VGG-11/vgg_numpy.py
+python3 -m pip install Code/PyPads_GPU_Details_Logger/dist/pypads-0.5.7_with_env_variable.tar.gz
+python3 Code/Logistic_Map/Logistic_Map_Script.py
+python3 Code/VGG-11/vgg_numpy.py
+
 
 # Now install pypads from dist
 echo "Script completed"
